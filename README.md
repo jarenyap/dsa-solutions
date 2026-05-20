0f6a08bef51931b364eb6ff76287a50f178b5db8
pompouslupus <227606516+jarenyap@users.noreply.github.com>
2026-05-20 12:51:02 +0800

    feat: updated repo folder structure and included solution for first neetcode.io problem

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # dsa-solutions
 My solutions to NeetCode 150 + Leetcode Problems
+
+## NeetCode-150
+| # | Problem | Difficulty | Topic | Solution |
+|---|---------|------------|-------|---------|
+| 1 | Contains Duplicate | Easy | Arrays, Hashing, Sorting | [link] (neetcode-150/easy/001-contains-duplicate.java) |
---
+++ neetcode-150/easy/001-contains-duplicate.java
@@ -0,0 +1,13 @@
+import java.util.Set;
+import java.util.HashSet;
+
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        Set<Integer> seen = new HashSet<>();
+        for (int num : nums) {
+            if (seen.contains(num)) { return true; }
+            seen.add(num);
+        }
+        return false;
+    }
+}
