64d4bb19761300d8dfb38824dc86df4d5a686fed
pompouslupus <227606516+jarenyap@users.noreply.github.com>
2026-05-23 16:44:05 +0800

    sol: solution to Balanced Binary Tree problem

---
+++ neetcode-150/easy/007-balanced-binary-tree.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        return dfs(root) != -1;
+    }
+
+    private int dfs(TreeNode node) {
+        if (node == null) { return 0; }
+        int leftHeight = dfs(node.left);
+        if (leftHeight == -1) { return -1; } // left is unbalanced, propogate
+        int rightHeight = dfs(node.right);
+        if (rightHeight == -1) { return -1; } // right is unbalanced, propogate
+
+        if (Math.abs(leftHeight - rightHeight) > 1) {
+            return -1;
+        } else {
+            return Math.max(leftHeight, rightHeight) + 1;
+        }
+    }
+}
